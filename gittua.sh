3001366ddb459d420ee39e0d28e922e824e4f0f3
TuringProblem <andrew.deca.wellington@gmail.com>
2025-02-11 22:30:08 -0500

    feat: Successfully added QuickSorting algorithm from DSA - refer to {java/wk4/}

---
+++ gittua.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+echo "Please enter your commit comment: "
+read commit
+git commit -m ${commit}
+echo "ready to push"
--- java/wk3/BSearchExample.java
+++ java/wk3/BSearchExample.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 
+/**
+ * @author { @Override } | 20250211 : @22:24
+ * @see <a href="https://github.com/TuringProblem/">GitHub Profile</a>
+ **/
+
 public class BSearchExample {
     public static int bSearch(int[] array, int maxLength, int value) {
         int l = 0;
         int h = maxLength - 1;
         while (h >= l) {
-            int m = (h + l) / 2; 
+            int m = (h + l) / 2;
             if (array[m] < value) {
-                l = m + 1;   
+                l = m + 1;
             } else if (array[m] > value) {
                 h = m - 1;
             } else {
                 return m;
             }
         }
-            return -1;
+        return -1;
     }
+
     public static int linearSearch(int[] arr, int value) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == value) {
@@ -25,9 +31,9 @@ public class BSearchExample {
         return -1;
     }
 
-
     public static void main(String[] args) {
-        int[] myValues = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
+        int[] myValues = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 };
         int maxLength = 40;
         System.out.println("Numbers: \n");
         for (int i = 0; i < maxLength; i++) {
@@ -36,12 +42,12 @@ public class BSearchExample {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a value: ");
         int userValue = scan.nextInt();
-        int keyIndex = bSearch(myValues, maxLength, userValue); 
+        int keyIndex = bSearch(myValues, maxLength, userValue);
         if (userValue == -1) {
             System.out.println("Please Try another value\n");
         } else {
             System.out.printf("Found the value {%d}\nAt index: %d\n", userValue, keyIndex);
-        } 
+        }
         System.out.println("Would you like to see this in a linear search?");
         String response = scan.next();
         if (response.equalsIgnoreCase("y")) {
