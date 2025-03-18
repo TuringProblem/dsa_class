d557d71dbd0161d8d01209e6e44daa0aa8558190
Override <122590935+TuringProblem@users.noreply.github.com>
2025-03-18 19:11:32 -0400

    dk

--- gittua.sh
+++ gittua.sh
@@ -4,6 +4,18 @@ echo "Please enter your commit comment: "
 read commit
 git commit -m "${commit}"
 echo "ready to push"
+
+function timeToPush() {
+  local input=$1
+  if [[ -v memo[$input] ]]; then
+    echo "Would you like to use ${input} as the output?"
+    read answer
+    if [[ $answer -eq 'y']]; then
+      return $input
+
+
+}
 echo "Please enter the url: "
 read url
+timeToPush $url
 git push ${url}
